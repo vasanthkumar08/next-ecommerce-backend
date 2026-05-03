9e84a5f38f3ce29fb5c02dff12451a199ef8adf3
Vasanth <kvasanthk372@gmail.com>
2026-05-03 05:46:01 +0530

    backend ready for deploy

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,13 @@ const app: Application = express();
 app.set("trust proxy", 1);
 app.use(corsMiddleware);
 app.options(/.*/, corsMiddleware);
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  return next();
+});
 app.use(helmet());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
